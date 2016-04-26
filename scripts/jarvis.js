64c397fb7ac315145763adc126aab0e17148f397
Jorge L. Torres A <jorgtowers@gmail.com>
2016-04-25 22:42:04 -0430

    Update jarvis.js

--- scripts/jarvis.js
+++ scripts/jarvis.js
@@ -85,6 +85,7 @@
             var bg = items[Math.floor(Math.random() * items.length)];
             var body = document.getElementsByTagName("body")[0];
             body.style.backgroundColor = "#" + bg;
+            body.style.color="white";
         },
         WebTimeline:function(){
             this.data=[];
