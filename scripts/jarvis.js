0ef23acf0ef1b22cd43f0edd50aa37a8c8dd20fb
Jorge L. Torres A <jorgtowers@gmail.com>
2016-05-16 23:54:40 -0400

    Update jarvis.js
    GUID

--- scripts/jarvis.js
+++ scripts/jarvis.js
@@ -659,6 +659,13 @@
         }
     }._();    
     Jarvis.prototype.Utils = {
+        GUID:function(){
+            function S4() {
+                return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+            }
+            var guid = (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toUpperCase();               
+            return guid;
+        },
         Autoguardado: {
             Guardar: function() {
                 if (_Tracert) {
