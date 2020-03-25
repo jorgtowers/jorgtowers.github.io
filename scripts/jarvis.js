f9f9679730692e6f5fa2d5571b6c27b8fafe5073
Jorge L. Torres A <jorgtowers@gmail.com>
2020-03-25 12:51:56 -0400

    Update jarvis.js

--- scripts/jarvis.js
+++ scripts/jarvis.js
@@ -312,20 +312,20 @@
                         },{
                             "title": "<b>Developer Senior</b>",
                             "companyName": "GIGA Automation & IT",
-                            "period": "Junio de 2017 – actualidad",
-                            "observation":"Desarrollado Senior en TAMSA (TENARIS), durando 2 anos, pude hacer varios proyectos en plataforma .net en lenguaje C# y HTML5 Javascript,  dentro de los mas destacados: \
+                            "period": "Junio de 2017 – Marzo de 2020",
+                            "observation":"Desarrollador Senior en TAMSA (TENARIS), participando en proyectos en plataforma .net en lenguaje C# y HTML5 Javascript,  dentro de los mas destacados: \
                                         <br>Servicio de Control de Tubos Recepción de material.\
                                         <br>Servicio de Yardas y ubicación de en google maps para ubicación de los contenedores en los patios de los almacenes.\
                                         <br>Servicio de integración con Microsoft Excel y  addins.\
                                         <br>Servicio de upload de data via XML a Web Services.\
                                         <br>y soportes en general de los desarrollos realizados"
-                        },{
+                        },/*{
                             "title": "<b>Director General de Tecnología</b>",
                             "companyName": "World Gold Corp, C.A.",
                             "period": "Octubre de 2017 – actualidad",
                             "observation":"Consultoria de sistemas\
                                         <br>Servicio de Tecnología de Información empresarial"
-                        }, {
+                        },*/ {
                             "title": "Gerente de Tecnología de Información",
                             "companyName": "Editorial Notitarde, C.A.",
                             "period": "Agosto de 2016 – Octubre de 2017",
@@ -335,12 +335,12 @@
                             "companyName": "Lourtec, S.A.",
                             "period": "Abril de 2016 – Noviembre de 2016",                            
                             "observation":"Instructor de carrera oficial 70-480"
-                        }, {
+                        },/* {
                             "title": "Supervisor de Desarrollo Web",
                             "companyName": "Editorial Notitarde, C.A.",
                             "period": "Mayo de 2004 – Agosto de 2016 (12 años)",                            
                             "observation":"Supervisor del area de desarrollo web"
-                        }, {
+                        }, */{
                             "title": "Instructor de C#",
                             "companyName": "Lourtec, S.A.",
                             "period": "Febrero de 2015 – Marzo de 2015 (2 meses)",                            
